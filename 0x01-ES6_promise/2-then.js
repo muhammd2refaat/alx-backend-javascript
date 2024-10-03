68ee981ffd938295cd5aebe5ae46194e6313ee82
muhammd2refaat <mohammed_refat@inbox.ru>
2024-10-03 03:16:38 +0300

    Add handleResponseFromAPI function with Promise

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,6 @@
+const handleResponseFromAPI = (promise) => promise
+  .then(() => ({ status: 200, body: 'success' }))
+  .catch(() => new Error())
+  .finally(() => console.log('Got a response from the API'));
+
+export default handleResponseFromAPI;
