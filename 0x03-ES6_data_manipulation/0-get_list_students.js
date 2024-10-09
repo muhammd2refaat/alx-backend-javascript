91e6625c9490d191262567734bd7d58c186c0542
muhammd2refaat <mohammed_refat@inbox.ru>
2024-10-09 05:38:28 +0300

    Refactor getListStudents function to use ES6 syntax and add JSDoc comments

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,14 +1,9 @@
-// Create a function named getListStudents that returns an array of objects.
-
-// Each object should have three attributes: id (Number), firstName (String), and location (String).
-
-// The array contains the following students in order:
-
-// Guillaume, id: 1, in San Francisco
-// James, id: 2, in Columbia
-// Serena, id: 5, in San Francisco
-
-const getListStudents = () => {
+/**
+ * Retrieves a list of students.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @returns {{id: Number, firstName: String, location: String}[]}
+ */
+export default function getListStudents() {
   return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
